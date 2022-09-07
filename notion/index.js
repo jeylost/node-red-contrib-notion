97cf3ea004034a31d7879c51527217028a33dab0
Yevhenii Medvedev <medvedev.gb@gmail.com>
2022-09-08 01:55:45 +0300

    fix: node-red doesn't support private fields for class

--- notion/index.js
+++ notion/index.js
@@ -2,9 +2,6 @@ const { Client } = require("@notionhq/client");
 
 module.exports = class Notion {
   constructor(notionKey) {
-    this.#client = new Client({ auth: notionKey });
+    this.client = new Client({ auth: notionKey });
   }
 }
-
-
-
