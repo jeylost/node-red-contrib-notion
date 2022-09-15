d81312ad9f28aa03c12cc86b0455e9e4be0cf703
Yevhenii Medvedev <medvedev.gb@gmail.com>
2022-09-16 02:06:55 +0300

    chore: add error logging for add page node

--- node-red/notion-add-page.js
+++ node-red/notion-add-page.js
@@ -11,6 +11,7 @@ module.exports = function(RED) {
       const { error, result } = notion.addPageToDatabase(msg.payload);
 
       if (error) {
+        this.log(error);
         throw error;
       }
 
